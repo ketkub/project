bb7222dededa02b21e3f9697fad03edda2469a08
ketkub <ketwowwow@gmail.com>
2024-10-25 17:09:23 +0700

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_product_app/screens/login_screen.dart';
 import 'package:flutter_product_app/screens/restaurant_list_screen.dart';
 
 void main() {
@@ -34,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: LoginScreen(),
+      home: RestaurantListScreen(),
     );
   }
 }
